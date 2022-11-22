6c028d0b0f077bb4583eb1c49861a5713e0da1c0
mhborthwick <mhborthwick@gmail.com>
2022-11-21 23:37:10 -0800

    chore: set up express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+import express from "express";
+import config from "config";
+
+const port = config.get<number>("port");
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Listening on - http://localhost:${port}`);
+});
