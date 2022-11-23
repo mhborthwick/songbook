9430f42f45252dc67d832337e015fcce9108e1f8
mhborthwick <mhborthwick@gmail.com>
2022-11-22 23:02:54 -0800

    chore: add logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import config from "config";
 import connect from "./utils/connect";
+import log from "./utils/logger";
 
 const port = config.get<number>("port");
 
 const app = express();
 
 app.listen(port, async () => {
-  console.log(`Listening on - http://localhost:${port}`);
+  log.info(`Listening on - http://localhost:${port}`);
   await connect();
 });
--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import config from "config";
+import log from "./logger";
 
 async function connect() {
   const dbUri = config.get<string>("dbUri");
   try {
     await mongoose.connect(dbUri);
-    console.log("Connected to DB");
+    log.info("Connected to DB");
   } catch (err) {
-    console.error("Failed connecting to DB");
+    log.error("Failed connecting to DB");
     process.exit(1);
   }
 }
