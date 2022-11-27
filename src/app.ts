8ced91f3c2cc3bb29cbc7e22a0ad8d075d7cbd66
mhborthwick <mhborthwick@gmail.com>
2022-11-27 01:48:59 -0800

    feat: add get sessions endpoint

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,14 @@ import config from "config";
 import connect from "./utils/connect";
 import log from "./utils/logger";
 import routes from "./routes";
+import deserializeUser from "./middleware/deserializeUser";
 
 const port = config.get<number>("port");
 
 const app = express();
 
 app.use(express.json());
+app.use(deserializeUser);
 
 app.listen(port, async () => {
   log.info(`Listening on - http://localhost:${port}`);
--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,6 +1,11 @@
-import Session from "../models/session.model";
+import { FilterQuery } from "mongoose";
+import Session, { SessionDocument } from "../models/session.model";
 
 export async function createSession(userId: string, userAgent: string) {
   const session = await Session.create({ user: userId, userAgent });
   return session.toJSON();
+}
+
+export async function findSessions(query: FilterQuery<SessionDocument>) {
+  return Session.find(query).lean();
 }
