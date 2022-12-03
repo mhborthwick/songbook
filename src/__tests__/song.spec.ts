ee16b1575651b3e916c7e43a4ae31ed9db32ccfd
mhborthwick <mhborthwick@gmail.com>
2022-12-03 15:25:23 -0800

    test: add another get song route test

--- src/__tests__/song.spec.ts
+++ src/__tests__/song.spec.ts
@@ -2,8 +2,14 @@ import supertest from "supertest";
 import createServer from "../utils/server";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import { createSong } from "../service/song.service";
 
 const app = createServer();
+const user = new mongoose.Types.ObjectId().toString();
+const songPayload = {
+  user,
+  url: "https://open.spotify.com/track/33yAEqzKXexYM3WlOYtTfQ",
+};
 
 describe("song", () => {
   beforeAll(async () => {
@@ -21,6 +27,17 @@ describe("song", () => {
       it("should return 404", async () => {
         const songId = "song-123";
         await supertest(app).get(`/api/songs/${songId}`).expect(404);
+      });
+    });
+
+    describe("given song does exist", () => {
+      it("should return 200 and song", async () => {
+        const song = await createSong(songPayload);
+        const { body, statusCode } = await supertest(app).get(
+          `/api/songs/${song.songId}`
+        );
+        expect(statusCode).toEqual(200);
+        expect(body.songId).toBe(song.songId);
       });
     });
   });
--- src/models/song.model.ts
+++ src/models/song.model.ts
@@ -10,6 +10,7 @@ export interface SongInput {
 }
 
 export interface SongDocument extends SongInput, mongoose.Document {
+  songId: string;
   createdAt: Date;
   updatedAt: Date;
 }
