d0c692e98603c9f06ee80f21759fe1fc82b14ced
Mike Borthwick <mhborthwick@gmail.com>
2023-06-26 17:16:25 -0700

    testing

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -31,6 +31,8 @@ import {
   updateUserPasswordSchema,
 } from "./schema";
 
+//test
+
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => {
     res.sendStatus(200);
